f8ec1a7424fbda350c2d05226c671c21d7cb903e
Kunalraj Singh <73094875+kunalraj0@users.noreply.github.com>
2023-02-28 12:00:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.static('public'))
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
