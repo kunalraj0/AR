1d16f2e709a0082943e1e42de91e2cb03112bf68
Kunalraj Singh <73094875+kunalraj0@users.noreply.github.com>
2023-02-28 11:41:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.static('public'))
-
-app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
